a61c1b37ce72fa15d9ed4b645f699faf69550403
Diego Cunha <diego2912@gmail.com>
2018-12-05 18:26:30 -0300

    update

--- modules/sparql.py
+++ modules/sparql.py
@@ -5,17 +5,20 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 def consutar_termos(uri, termos, idioma):
 	sparql = SPARQLWrapper(uri)
-	#list_querys = []
 	list_querys = {}
 	try:
 		for t in termos:
 			query = """
+					PREFIX dbr: <http://dbpedia.org/resource/>
+					PREFIX dbp: <http://dbpedia.org/property/>	
+					PREFIX dct: <http://purl.org/dc/terms/>
+					
 					SELECT DISTINCT(?subject) ?termo
 					WHERE
 					{
-						?subject dct:subject ?comp .
+						?subject dct:subject dbc:Computer_systems_researchers .
 						?subject dbo:abstract ?termo
-						FILTER (regex(str(?comp), "Comput", "i") && regex(?termo, "%s", "i"))
+						FILTER (regex(?termo, "%s", "i"))
 						FILTER (LANG(?termo) = "%s")
 					} LIMIT 1
 					"""%(t, idioma)
@@ -37,36 +40,3 @@ def parse(value):
 		r['uri'] = v['subject']['value']
 		r['abstract'] = v['termo']['value']
 	return r
-		
-	
-'''
-sparql = SPARQLWrapper('http://dbpedia.org/sparql')
-
-query = """
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-PREFIX dc: <http://purl.org/dc/elements/1.1/>
-PREFIX : <http://dbpedia.org/resource/>
-PREFIX dbpedia2: <http://dbpedia.org/property/>
-PREFIX dbpedia: <http://dbpedia.org/>
-PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-
-SELECT DISTINCT(?subject) ?termo
-WHERE
-{
-    ?subject dct:subject ?comp .
-    ?subject dbo:abstract ?termo
-    FILTER (regex(?comp, "Comput", "i") && regex(?termo, "%s", "i"))
-    FILTER (LANG(?termo) = "%s")
-} LIMIT 1
-"""%('MINIX', 'en')
-
-sparql.setQuery(query)
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
-
-print(results['results']['bindings'])
-'''
